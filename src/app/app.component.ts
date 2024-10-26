010f5e786479ae3c2c00c5ac794ad388fb03ec70
IEVN1003-20001612 <20001612@alumnos.utleon.edu.mx>
2024-10-25 18:47:34 -0600

    25/10/24

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TempComponent } from './tem/temp/temp.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, TempComponent, NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angularApi';
+}
---
+++ src/app/tem/temp/temp.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { TemhComponent } from '../temh/temh.component';
+
+@Component({
+  selector: 'app-temp',
+  standalone: true,
+  imports: [TemhComponent],
+  templateUrl: './temp.component.html',
+  styles: ``
+})
+export class TempComponent {
+
+  title="Saludo desde el Componente Padre"
+
+}
